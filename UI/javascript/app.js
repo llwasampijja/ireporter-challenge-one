6b92d9729748e6c36a77a7b37cf8d55b139c279d
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-12-17 13:24:57 +0300

    Added view page for the red-flags/intervention. [Delivers #162697038]

--- UI/javascript/app.js
+++ UI/javascript/app.js
@@ -27,3 +27,36 @@ function openSigninPage() {
 function openHomePage() {
     location.href = 'my_reports.html'
 }
+
+
+// #########################################
+function openViewReportsModal() {
+    // Get the modal
+    var modal = document.getElementById('view-report-details-modal');
+
+    // Get the buttons that open the modal
+    var btn_ = document.getElementsByClassName("view-report-btn");
+
+    document.addEventListener('click', function (event) {
+        if (event.target.classList.contains('view-report-btn')) {
+            modal.style.display = "block";
+        }
+    }, false);
+
+    // Get the <span> element which closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Close modal whe a user clicks the "X" button
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    //Close modal when a user clicks anywhere outside it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+// ############################################
