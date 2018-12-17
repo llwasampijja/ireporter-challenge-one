c8491bf3183962882631fbf4a20099844e6022c0
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-12-17 17:36:40 +0300

    Created "create new red-flag/intervention" page [Delivers #162702010]

--- UI/javascript/app.js
+++ UI/javascript/app.js
@@ -62,9 +62,6 @@ function openEditReportsModal() {
     // Get the modal
     var modal = document.getElementById('edit-report-details-modal');
 
-    // Get the buttons that open the modal
-    // var btn_ = document.getElementsByClassName("edit-report-btn");
-
     document.addEventListener('click', function (event) {
         if (event.target.classList.contains('edit-report-btn')) {
             modal.style.display = "block";
@@ -97,27 +94,64 @@ function openEditReportsModal() {
     }
 }
 
-// function getUserGeoCordinates() {
-    // var x = document.getElementById("geocoordinates-field");
+function createNewReportsModal() {
+    // Get the modal
+    var modal = document.getElementById('create-new-report-modal');
 
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            x.innerHTML = "Geolocation is not supported by your current browser.";
+    var create_new_report = document.getElementById('new-report-btn')
+
+    create_new_report.addEventListener ('click', function (event) {
+        modal.style.display = "block"
+    });
+
+    // document.addEventListener('click', function (event) {
+    //     if (event.target.classList.contains('edit-report-btn')) {
+    //         modal.style.display = "block";
+    //     }
+    // }, false);
+
+    // Get the <span> element which closes the modal
+    var span = document.getElementsByClassName("close")[2];
+    var cancel_create_report = document.getElementById("cancel-create-report")
+    var save_create_report = document.getElementById("save-create-report")
+
+    // Close modal when a user clicks the "X" button
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    cancel_create_report.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    save_create_report.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    //Close modal when a user clicks anywhere outside it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
     }
+}
 
-    function showPosition(position) {
-        // x.innerHTML = "Latitude: " + position.coords.latitude +
-        //     "<br>Longitude: " + position.coords.longitude;
-
-            document.getElementById('geocoordinates-field').innerHTML = position.coords.latitude + "," + position.coords.longitude;
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by your current browser.";
     }
-// }
+}
+
+function showPosition(position) {
+
+    document.getElementById('geocoordinates-field').innerHTML = position.coords.latitude + "," + position.coords.longitude;
+}
 
 
 function runAllJavaScript() {
     openViewReportsModal();
     openEditReportsModal();
+    createNewReportsModal();
 } 
